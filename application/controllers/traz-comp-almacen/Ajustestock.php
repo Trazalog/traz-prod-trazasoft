2cacaf6ec3c1e1ccc1881e4e360515a5e55a83d6
fernando <fguardia@trazalog.com>
2019-11-21 00:40:38 -0300

    INICIO DE FER

---
+++ application/controllers/traz-comp-almacen/Ajustestock.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Ajustestock extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        //$this->load->model(ALM . 'Articulos');
+    }
+    public function index()
+    {
+        echo 'Componente Almacen<br>';  
+    }
+
+    public function install()
+    {
+        #$this->Ala
+    }
+}
--- application/views/layout/aux_menu_alm.php
+++ application/views/layout/aux_menu_alm.php
@@ -22,6 +22,8 @@
                         class="fa fa-fw fa-cart-plus"></i>Pedido Materiales</a></li>
             <li><a href="#" class="link" data-link="traz-comp-almacen/Proceso"><i class="fa fa-cogs"></i>Mis Tareas</a>
             </li>
+            <li><a href="#" class="link" data-link="traz-comp-almacen/Ajustestock"><i class="fa fa-wrench"></i>Ajuste de Stock</a>
+            </li>
         </ul>
     </li>
 </ul>
