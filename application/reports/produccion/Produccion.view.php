9696c0d7aaac17ba8544ab9204ec6bcce961ec4e
Nacho <mestrenacho1991@gmail.com>
2020-01-03 15:03:55 -0300

    KoolReport
    Standard Maquetacion de reportes ( sin CSS)

--- application/reports/produccion/Produccion.view.php
+++ application/reports/produccion/Produccion.view.php
@@ -8,115 +8,214 @@ use \koolreport\widgets\google\PieChart;
 
 ?>
 
+
+
+
+
 <body>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box box-solid">
-                <div id="reportContent" class="report-content">
-                    <div>
-                        <a href="#" data-toggle="control-sidebar" title="Filtros">
-                            <i class="fa fa-fw fa-filter fa-lg text-black"></i>
-                        </a>
-                    </div>
-                    <div class="box-body">
-                        <?php
-                        Table::create(array(
-                            "dataStore" => $this->dataStore('data_produccion_table'),
-                            // "themeBase" => "bs4",
-                            // "showFooter" => true, // cambiar true por "top" para ubicarlo en la parte superior
-                            "headers" => array(
-                                array(
-                                    "Reporte de Producción" => array("colSpan" => 6),
-                                    // "Other Information" => array("colSpan" => 2),
-                                )
-                            ), // Para desactivar encabezado reemplazar "headers" por "showHeader"=>false
-                            "columns" => array(
-                                "fecha" => array(
-                                    "label" => "Fecha"
-                                ),
-                                "producto" => array(
-                                    "label" => "Producto"
-                                ),
-                                "cantidad" => array(
-                                    "label" => "Cantidad"
-                                ),
-                                "unidad_medida" => array(
-                                    "label" => "Unidad Medida"
-                                ),
-                                "etapa" => array(
-                                    "label" => "Etapa"
-                                ),
-                                "recurso" => array(
-                                    "label" => "Responsable"
-                                )
-                            ),
-                            "cssClass" => array(
-                                // "table" => "table-bordered table-striped table-hover dataTable",
-                                "table" => "table-striped table-hover dataTables_wrapper form-inline dt-bootstrap"
-                                // "th" => "sorting"
-                                // "tr" => "cssItem"
-                                // "tf" => "cssFooter"
-                            )
-                        ));
-                        ?>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="box-body">
-                                <div style="margin-bottom:50px;">
-                                    <?php
-                                    PieChart::create(array(
-                                        "title" => "Cantidad de productos",
-                                        "dataStore" => $this->dataStore('data_produccion_pieChart'),
-                                        "columns" => array(
-                                            "producto",
-                                            "cantidad" => array(
-                                                "type" => "number",
-                                            )
-                                        ),
-                                        "colorScheme" => array(
-                                            "#2f4454",
-                                            "#2e1518",
-                                            "#da7b93",
-                                            "#376e6f",
-                                            "#1c3334"
-                                        )
-                                    ));
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="box-body">
-                                <div style="margin-bottom:50px;">
-                                    <?php
-                                    ColumnChart::create(array(
-                                        "title" => "Productos con mayor cantidad",
-                                        "dataStore" => $this->dataStore('data_produccion_pieChart'),
-                                        "columns" => array(
-                                            "producto",
-                                            "cantidad" => array(
-                                                "type" => "number",
-                                                "label" => "Cantidad"
-                                            )
-                                        ),
-                                        "colorScheme" => array(
-                                            "#2f4454",
-                                            "#2e1518",
-                                            "#da7b93",
-                                            "#376e6f",
-                                            "#1c3334"
-                                        )
-                                    ));
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+
+<div class="row">
+
+    <div class="col-md-12">
+
+        
+        <div class="box box-solid">
+
+        <div class="box box-primary">
+
+            <div class="box-header">
+                <h3 class="box-title">
+                    <i class="fa fa-list"></i>
+
+                    Reportes
+                </h3>
             </div>
+
+            <div class="col-md-12"><hr></div>
+
+        
+
+                    <!--_________________BODY REPORTE___________________________-->
+
+                    <div id="reportContent" class="report-content">
+
+                        <!--_________________FILTRO_________________-->
+
+                        <div class="col-md-12">
+                            <a href="#" data-toggle="control-sidebar" title="Filtros">
+                                <i class="fa fa-fw fa-filter fa-lg text-black pull-left"></i>
+                            </a>
+                        </div>
+
+                        <div class="col-md-12"><br></div>
+
+                        <!--_________________TABLA_________________-->
+
+                        <div class="box-body">
+
+                            <div class="col-md-12">
+                                
+                                <?php
+                                Table::create(array(
+                                    "dataStore" => $this->dataStore('data_produccion_table'),
+                                    // "themeBase" => "bs4",
+                                    // "showFooter" => true, // cambiar true por "top" para ubicarlo en la parte superior
+                                    "headers" => array(
+                                        array(
+                                            "Reporte de Producción" => array("colSpan" => 6),
+                                            // "Other Information" => array("colSpan" => 2),
+                                        )
+                                    ), // Para desactivar encabezado reemplazar "headers" por "showHeader"=>false
+                                    "columns" => array(
+                                        "fecha" => array(
+                                            "label" => "Fecha"
+                                        ),
+                                        "producto" => array(
+                                            "label" => "Producto"
+                                        ),
+                                        "cantidad" => array(
+                                            "label" => "Cantidad"
+                                        ),
+                                        "unidad_medida" => array(
+                                            "label" => "Unidad Medida"
+                                        ),
+                                        "etapa" => array(
+                                            "label" => "Etapa"
+                                        ),
+                                        "recurso" => array(
+                                            "label" => "Responsable"
+                                        )
+                                    ),
+                                    "cssClass" => array(
+                                        // "table" => "table-bordered table-striped table-hover dataTable",
+                                        "table" => "table-striped table-scroll table-hover  table-responsive dataTables_wrapper form-inline table-scroll table-responsive dt-bootstrap dataTable",
+                                        "th" => "sorting"
+                                        // "tr" => "cssItem"
+                                        // "tf" => "cssFooter"
+                                    )
+                                ));
+                                ?>
+
+                            </div>
+                        </div>
+
+                        <!--_________________FIN TABLA_________________-->
+
+
+                        <div class="col-md-12">
+                        <br>
+                            <div class="box box-primary">
+                            </div>
+                        </div>
+
+                        <!--_________________ CHARTS_________________-->
+
+                        <div class="row">
+                        <div class="col-md-12">
+
+                            <!--_________________ CHARTS 1_________________-->
+                            <div class="col-md-6">
+
+
+                            <div class="box-header">
+                                <h3 class="box-title">
+                                    <i class="fa fa-pie-chart"></i>
+                                    Titulo Chart
+                                </h3>
+                            </div>
+
+                            <!--_________________ BODY CHART 1_________________-->
+
+                                <div class="box-body">
+                                    <div style="margin-bottom:50px;">
+                                        <?php
+                                        PieChart::create(array(
+                                            "title" => "Cantidad de productos",
+                                            "dataStore" => $this->dataStore('data_produccion_pieChart'),
+                                            "columns" => array(
+                                                "producto",
+                                                "cantidad" => array(
+                                                    "type" => "number",
+                                                )
+                                            ),
+                                            "colorScheme" => array(
+                                                "#2f4454",
+                                                "#2e1518",
+                                                "#da7b93",
+                                                "#376e6f",
+                                                "#1c3334"
+                                            )
+                                        ));
+                                        ?>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+
+                            <!--_________________ CHARTS 2_________________-->
+
+
+                            <div class="col-md-6">
+
+                                    <div class="box-header">
+                                        <h3 class="box-title">
+                                            <i class="fa fa-pie-chart"></i>
+                                            Titulo Chart 2
+                                        </h3>
+                                    </div>
+
+                                    <!--_________________ BODY CHART 2_________________-->
+
+                                <div class="box-body">
+                                    <div style="margin-bottom:50px;">
+                                        <?php
+                                        ColumnChart::create(array(
+                                            "title" => "Productos con mayor cantidad",
+                                            "dataStore" => $this->dataStore('data_produccion_pieChart'),
+                                            "columns" => array(
+                                                "producto",
+                                                "cantidad" => array(
+                                                    "type" => "number",
+                                                    "label" => "Cantidad"
+                                                )
+                                            ),
+                                            "colorScheme" => array(
+                                                "#2f4454",
+                                                "#2e1518",
+                                                "#da7b93",
+                                                "#376e6f",
+                                                "#1c3334"
+                                            )
+                                        ));
+                                        ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--_________________ FIN CHARTS_________________-->
+
+                        
+
+                            
+
+
+
+                    </div>
+
+
+                    <!--_________________ FIN BODY REPORTE ____________________________-->
+
+                    </div>
         </div>
+
     </div>
+
+</div>
     <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
@@ -135,4 +234,9 @@ use \koolreport\widgets\google\PieChart;
 
         $('#panel-derecho-body').load('<?php echo base_url() ?>index.php/Reportes/filtroProduccion');
     </script>
+
+<script>
+
+DataTable($('.dataTable'))
+        </script>
 </body>
--- application/views/layout/general_scripts.php
+++ application/views/layout/general_scripts.php
@@ -28,11 +28,14 @@ $.widget.bridge('uibutton', $.ui.button);
        <!-- FastClick -->
        <script src="<?php echo base_url()?>lib/bower_components/fastclick/lib/fastclick.js"></script>
 
+
+        <!-- Datatbles -->
+
        <script src="<?php echo base_url()?>lib/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 
-       <script src="<?php echo base_url()?>lib/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js">
-       </script>
-       <!-- AdminLTE App -->
+       <script src="<?php echo base_url()?>lib/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+     
+     <!-- AdminLTE App -->
        <script src="<?php echo base_url()?>lib/dist/js/adminlte.min.js"></script>
        <script type="text/javascript"
            src="//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/js/bootstrapValidator.min.js"></script>
