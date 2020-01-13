095692808948a0521ded2c027f09fce03dec313a
mbustos <mbustos@trazalog.com>
2020-01-13 15:00:07 -0300

    Koolreport: prod responsable, log,

--- application/reports/prodResponsable/Prod_Responsable.view.php
+++ application/reports/prodResponsable/Prod_Responsable.view.php
@@ -10,107 +10,207 @@ use \koolreport\widgets\google\PieChart;
 
 <body>
     <div class="row">
+
         <div class="col-md-12">
+
+
             <div class="box box-solid">
-                <div id="reportContent" class="report-content">
-                    <div>
-                        <a href="#" data-toggle="control-sidebar" title="Filtros">
-                            <i class="fa fa-fw fa-filter fa-lg text-black"></i>
-                        </a>
+
+                <div class="box box-primary">
+
+                    <div class="box-header">
+                        <h3 class="box-title">
+                            <i class="fa fa-list"></i>
+
+                            Reportes
+                        </h3>
                     </div>
-                    <div class="box-body">
-                        <?php
-                        Table::create(array(
-                            "dataStore" => $this->dataStore('data_prodResponsable_table'),
-                            "headers" => array(
-                                array(
-                                    "Reporte de Producción por Recurso" => array("colSpan" => 6),
-                                )
-                            ), // Para desactivar encabezado reemplazar "headers" por "showHeader"=>false
-                            "columns" => array(
-                                "fecha" => array(
-                                    "label" => "Fecha"
-                                ),
-                                "producto" => array(
-                                    "label" => "Producto"
-                                ),
-                                "cantidad" => array(
-                                    "label" => "Cantidad"
-                                ),
-                                "unidad_medida" => array(
-                                    "label" => "Unidad Medida"
-                                ),
-                                "etapa" => array(
-                                    "label" => "Etapa"
-                                ),
-                                "recurso" => array(
-                                    "label" => "Responsable"
-                                )
-                            ),
-                            "cssClass" => array(
-                                "table" => "table-striped table-hover dataTables_wrapper form-inline dt-bootstrap"
-                                // "th" => "sorting"
-                                // "tr" => "cssItem"
-                                // "tf" => "cssFooter"
-                            )
-                        ));
-                        ?>
+
+                    <div class="col-md-12">
+                        <hr>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="box-body">
-                                <div style="margin-bottom:50px;">
-                                    <?php
-                                    PieChart::create(array(
-                                        "title" => "Cantidad de productos",
-                                        "dataStore" => $this->dataStore('data_prodResponsable_pieChart'),
-                                        "columns" => array(
-                                            "producto",
-                                            "cantidad" => array(
-                                                "type" => "number",
-                                            )
-                                        ),
-                                        "colorScheme" => array(
-                                            "#2f4454",
-                                            "#2e1518",
-                                            "#da7b93",
-                                            "#376e6f",
-                                            "#1c3334"
+
+
+
+                    <!--_________________BODY REPORTE___________________________-->
+
+                    <div id="reportContent" class="report-content">
+
+                        <!--_________________FILTRO_________________-->
+
+                        <div class="col-md-12">
+                            <a href="#" data-toggle="control-sidebar" title="Filtros">
+                                <i class="fa fa-fw fa-filter fa-lg text-black pull-left"></i>
+                            </a>
+                        </div>
+
+                        <div class="col-md-12"><br></div>
+
+                        <!--_________________TABLA_________________-->
+
+                        <div class="box-body">
+
+                            <div class="col-md-12">
+
+                                <?php
+                                Table::create(array(
+                                    "dataStore" => $this->dataStore('data_prodResponsable_table'),
+                                    // "themeBase" => "bs4",
+                                    // "showFooter" => true, // cambiar true por "top" para ubicarlo en la parte superior
+                                    "headers" => array(
+                                        array(
+                                            "Reporte de Producción por Recurso" => array("colSpan" => 6),
+                                            // "Other Information" => array("colSpan" => 2),
                                         )
-                                    ));
-                                    ?>
-                                </div>
+                                    ), // Para desactivar encabezado reemplazar "headers" por "showHeader"=>false
+                                    "columns" => array(
+                                        "fecha" => array(
+                                            "label" => "Fecha"
+                                        ),
+                                        "producto" => array(
+                                            "label" => "Producto"
+                                        ),
+                                        "cantidad" => array(
+                                            "label" => "Cantidad"
+                                        ),
+                                        "unidad_medida" => array(
+                                            "label" => "Unidad Medida"
+                                        ),
+                                        "etapa" => array(
+                                            "label" => "Etapa"
+                                        ),
+                                        "recurso" => array(
+                                            "label" => "Responsable"
+                                        )
+                                    ),
+                                    "cssClass" => array(
+                                        // "table" => "table-bordered table-striped table-hover dataTable",
+                                        "table" => "table-striped table-scroll table-hover  table-responsive dataTables_wrapper form-inline table-scroll table-responsive dt-bootstrap dataTable",
+                                        "th" => "sorting"
+                                        // "tr" => "cssItem"
+                                        // "tf" => "cssFooter"
+                                    )
+                                ));
+                                ?>
+
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="box-body">
-                                <div style="margin-bottom:50px;">
-                                    <?php
-                                    ColumnChart::create(array(
-                                        "title" => "Productos con mayor cantidad",
-                                        "dataStore" => $this->dataStore('data_prodResponsable_pieChart'),
-                                        "columns" => array(
-                                            "producto",
-                                            "cantidad" => array(
-                                                "type" => "number",
-                                                "label" => "Cantidad"
-                                            )
-                                        ),
-                                        "colorScheme" => array(
-                                            "#2f4454",
-                                            "#2e1518",
-                                            "#da7b93",
-                                            "#376e6f",
-                                            "#1c3334"
-                                        )
-                                    ));
-                                    ?>
-                                </div>
+
+                        <!--_________________FIN TABLA_________________-->
+
+
+                        <div class="col-md-12">
+                            <br>
+                            <div class="box box-primary">
                             </div>
                         </div>
+
+                        <!--_________________ CHARTS_________________-->
+
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <!--_________________ CHARTS 1_________________-->
+                                <div class="col-md-6">
+
+
+                                    <div class="box-header">
+                                        <h3 class="box-title center">
+                                            <i class="fa fa-pie-chart"></i>
+                                            Cantidad de productos
+                                        </h3>
+                                    </div>
+
+                                    <!--_________________ BODY CHART 1_________________-->
+
+                                    <div class="box-body">
+                                        <div style="margin-bottom:50px;">
+                                            <?php
+                                            PieChart::create(array(
+                                                // "title" => "Cantidad de productos",
+                                                "dataStore" => $this->dataStore('data_prodResponsable_pieChart'),
+                                                "columns" => array(
+                                                    "producto",
+                                                    "cantidad" => array(
+                                                        "type" => "number",
+                                                    )
+                                                ),
+                                                "colorScheme" => array(
+                                                    "#2f4454",
+                                                    "#2e1518",
+                                                    "#da7b93",
+                                                    "#376e6f",
+                                                    "#1c3334"
+                                                )
+                                            ));
+                                            ?>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+
+
+                                <!--_________________ CHARTS 2_________________-->
+
+
+                                <div class="col-md-6">
+
+                                    <div class="box-header">
+                                        <h3 class="box-title">
+                                            <i class="fa fa-pie-chart"></i>
+                                            Productos con mayor cantidad
+                                        </h3>
+                                    </div>
+
+                                    <!--_________________ BODY CHART 2_________________-->
+
+                                    <div class="box-body">
+                                        <div style="margin-bottom:50px;">
+                                            <?php
+                                            ColumnChart::create(array(
+                                                // "title" => "Productos con mayor cantidad",
+                                                "dataStore" => $this->dataStore('data_prodResponsable_pieChart'),
+                                                "columns" => array(
+                                                    "producto",
+                                                    "cantidad" => array(
+                                                        "type" => "number",
+                                                        "label" => "Cantidad"
+                                                    )
+                                                ),
+                                                "colorScheme" => array(
+                                                    "#2f4454",
+                                                    "#2e1518",
+                                                    "#da7b93",
+                                                    "#376e6f",
+                                                    "#1c3334"
+                                                )
+                                            ));
+                                            ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--_________________ FIN CHARTS_________________-->
+
+
+
+
+
+
+
+                        </div>
+
+
+                        <!--_________________ FIN BODY REPORTE ____________________________-->
+
                     </div>
                 </div>
+
             </div>
+
         </div>
     </div>
 
@@ -123,5 +223,7 @@ use \koolreport\widgets\google\PieChart;
         });
 
         $('#panel-derecho-body').load('<?php echo base_url() ?>index.php/Reportes/filtroProdResponsable');
+
+        DataTable($('.dataTable'))
     </script>
 </body>
--- application/views/layout/Filtro.php
+++ application/views/layout/Filtro.php
@@ -81,7 +81,7 @@
                         <!-- <input type="text" value="-100,100" class="slider form-control" data-slider-min="-200" data-slider-max="200" data-slider-step="5" data-slider-value="[-100,100]" data-slider-orientation="horizontal" data-slider-selection="before" data-slider-tooltip="show" data-slider-id="green" data-value="-100,100" style="display: none;"> -->
                     <?php } ?>
                     <br>
-                    <div class="form-group">
+                    <!-- <div class="form-group">
                         <label>Rango Fechas:</label>
                         <div class="input-group">
                         <button type="button" class="btn btn-default pull-right form-control" id="daterange-btn">
@@ -91,7 +91,7 @@
                             <i class="fa fa-caret-down"></i>
                         </button>
                         </div>
-                    </div>                    
+                    </div>                     -->
                     <!-- <div class="form-group">
                     <a href='#' class='flt-clear pull-right text-red'><small><i class='fa fa-trash-o'></i> Limpiar Todo</small></a>
                 </div>
@@ -119,27 +119,27 @@
     // $('.select2').select2()
 
     //Date range picker
-    $('#reservation').daterangepicker()
+    // $('#reservation').daterangepicker()
     //Date range picker with time picker
-    $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, locale: { format: 'MM/DD/YYYY hh:mm A' }})
+    // $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, locale: { format: 'MM/DD/YYYY hh:mm A' }})
     //Date range as a button
-    $('#daterange-btn').daterangepicker(
-      {
-        ranges   : {
-          'Hoy'       : [moment(), moment()],
-          'Ayer'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Últimos 7 días' : [moment().subtract(6, 'days'), moment()],
-          'Últimos 30 días': [moment().subtract(29, 'days'), moment()],
-          'Este mes'  : [moment().startOf('month'), moment().endOf('month')],
-          'Último mes'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        startDate: moment().subtract(29, 'days'),
-        endDate  : moment()
-      },
-      function (start, end) {
-        $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-      }
-    )
+    // $('#daterange-btn').daterangepicker(
+    //   {
+    //     ranges   : {
+    //       'Hoy'       : [moment(), moment()],
+    //       'Ayer'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+    //       'Últimos 7 días' : [moment().subtract(6, 'days'), moment()],
+    //       'Últimos 30 días': [moment().subtract(29, 'days'), moment()],
+    //       'Este mes'  : [moment().startOf('month'), moment().endOf('month')],
+    //       'Último mes'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    //     },
+    //     startDate: moment().subtract(29, 'days'),
+    //     endDate  : moment()
+    //   },
+    //   function (start, end) {
+    //     $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+    //   }
+    // )
 
     $('.datepicker').datepicker({
         autoclose: true
@@ -181,200 +181,3 @@
         });
     }
 </script>
-<!-- <div class="datepicker datepicker-dropdown dropdown-menu datepicker-orient-left datepicker-orient-bottom" style="top: 491px; left: 810px; z-index: 10; display: block;">
-    <div class="datepicker-days" style="">
-        <table class="table-condensed">
-            <thead>
-                <tr>
-                    <th colspan="7" class="datepicker-title" style="display: none;"></th>
-                </tr>
-                <tr>
-                    <th class="prev">«</th>
-                    <th colspan="5" class="datepicker-switch">March 2019</th>
-                    <th class="next">»</th>
-                </tr>
-                <tr>
-                    <th class="dow">Su</th>
-                    <th class="dow">Mo</th>
-                    <th class="dow">Tu</th>
-                    <th class="dow">We</th>
-                    <th class="dow">Th</th>
-                    <th class="dow">Fr</th>
-                    <th class="dow">Sa</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="old day" data-date="1550966400000">24</td>
-                    <td class="old day" data-date="1551052800000">25</td>
-                    <td class="old day" data-date="1551139200000">26</td>
-                    <td class="old day" data-date="1551225600000">27</td>
-                    <td class="old day" data-date="1551312000000">28</td>
-                    <td class="day" data-date="1551398400000">1</td>
-                    <td class="day" data-date="1551484800000">2</td>
-                </tr>
-                <tr>
-                    <td class="day" data-date="1551571200000">3</td>
-                    <td class="day" data-date="1551657600000">4</td>
-                    <td class="day" data-date="1551744000000">5</td>
-                    <td class="day" data-date="1551830400000">6</td>
-                    <td class="day" data-date="1551916800000">7</td>
-                    <td class="day" data-date="1552003200000">8</td>
-                    <td class="day" data-date="1552089600000">9</td>
-                </tr>
-                <tr>
-                    <td class="day" data-date="1552176000000">10</td>
-                    <td class="day" data-date="1552262400000">11</td>
-                    <td class="day" data-date="1552348800000">12</td>
-                    <td class="day" data-date="1552435200000">13</td>
-                    <td class="day" data-date="1552521600000">14</td>
-                    <td class="day" data-date="1552608000000">15</td>
-                    <td class="day" data-date="1552694400000">16</td>
-                </tr>
-                <tr>
-                    <td class="day" data-date="1552780800000">17</td>
-                    <td class="day" data-date="1552867200000">18</td>
-                    <td class="day" data-date="1552953600000">19</td>
-                    <td class="day" data-date="1553040000000">20</td>
-                    <td class="day" data-date="1553126400000">21</td>
-                    <td class="day" data-date="1553212800000">22</td>
-                    <td class="day" data-date="1553299200000">23</td>
-                </tr>
-                <tr>
-                    <td class="day" data-date="1553385600000">24</td>
-                    <td class="day" data-date="1553472000000">25</td>
-                    <td class="day" data-date="1553558400000">26</td>
-                    <td class="day" data-date="1553644800000">27</td>
-                    <td class="day" data-date="1553731200000">28</td>
-                    <td class="day" data-date="1553817600000">29</td>
-                    <td class="day" data-date="1553904000000">30</td>
-                </tr>
-                <tr>
-                    <td class="day" data-date="1553990400000">31</td>
-                    <td class="new day" data-date="1554076800000">1</td>
-                    <td class="new day" data-date="1554163200000">2</td>
-                    <td class="new day" data-date="1554249600000">3</td>
-                    <td class="new day" data-date="1554336000000">4</td>
-                    <td class="new day" data-date="1554422400000">5</td>
-                    <td class="new day" data-date="1554508800000">6</td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th colspan="7" class="today" style="display: none;">Today</th>
-                </tr>
-                <tr>
-                    <th colspan="7" class="clear" style="display: none;">Clear</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-    <div class="datepicker-months" style="display: none;">
-        <table class="table-condensed">
-            <thead>
-                <tr>
-                    <th colspan="7" class="datepicker-title" style="display: none;"></th>
-                </tr>
-                <tr>
-                    <th class="prev">«</th>
-                    <th colspan="5" class="datepicker-switch">2019</th>
-                    <th class="next">»</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td colspan="7"><span class="month">Jan</span><span class="month">Feb</span><span class="month focused">Mar</span><span class="month">Apr</span><span class="month">May</span><span class="month">Jun</span><span class="month">Jul</span><span class="month">Aug</span><span class="month">Sep</span><span class="month">Oct</span><span class="month">Nov</span><span class="month">Dec</span></td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th colspan="7" class="today" style="display: none;">Today</th>
-                </tr>
-                <tr>
-                    <th colspan="7" class="clear" style="display: none;">Clear</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-    <div class="datepicker-years" style="display: none;">
-        <table class="table-condensed">
-            <thead>
-                <tr>
-                    <th colspan="7" class="datepicker-title" style="display: none;"></th>
-                </tr>
-                <tr>
-                    <th class="prev">«</th>
-                    <th colspan="5" class="datepicker-switch">2010-2019</th>
-                    <th class="next">»</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td colspan="7"><span class="year old">2009</span><span class="year">2010</span><span class="year">2011</span><span class="year">2012</span><span class="year">2013</span><span class="year">2014</span><span class="year">2015</span><span class="year">2016</span><span class="year">2017</span><span class="year">2018</span><span class="year focused">2019</span><span class="year new">2020</span></td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th colspan="7" class="today" style="display: none;">Today</th>
-                </tr>
-                <tr>
-                    <th colspan="7" class="clear" style="display: none;">Clear</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-    <div class="datepicker-decades" style="display: none;">
-        <table class="table-condensed">
-            <thead>
-                <tr>
-                    <th colspan="7" class="datepicker-title" style="display: none;"></th>
-                </tr>
-                <tr>
-                    <th class="prev">«</th>
-                    <th colspan="5" class="datepicker-switch">2000-2090</th>
-                    <th class="next">»</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td colspan="7"><span class="decade old">1990</span><span class="decade">2000</span><span class="decade focused">2010</span><span class="decade">2020</span><span class="decade">2030</span><span class="decade">2040</span><span class="decade">2050</span><span class="decade">2060</span><span class="decade">2070</span><span class="decade">2080</span><span class="decade">2090</span><span class="decade new">2100</span></td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th colspan="7" class="today" style="display: none;">Today</th>
-                </tr>
-                <tr>
-                    <th colspan="7" class="clear" style="display: none;">Clear</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-    <div class="datepicker-centuries" style="display: none;">
-        <table class="table-condensed">
-            <thead>
-                <tr>
-                    <th colspan="7" class="datepicker-title" style="display: none;"></th>
-                </tr>
-                <tr>
-                    <th class="prev">«</th>
-                    <th colspan="5" class="datepicker-switch">2000-2900</th>
-                    <th class="next">»</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td colspan="7"><span class="century old">1900</span><span class="century focused">2000</span><span class="century">2100</span><span class="century">2200</span><span class="century">2300</span><span class="century">2400</span><span class="century">2500</span><span class="century">2600</span><span class="century">2700</span><span class="century">2800</span><span class="century">2900</span><span class="century new">3000</span></td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th colspan="7" class="today" style="display: none;">Today</th>
-                </tr>
-                <tr>
-                    <th colspan="7" class="clear" style="display: none;">Clear</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
-</div> -->
