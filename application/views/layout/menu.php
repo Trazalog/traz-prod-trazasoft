9f4a2ca771308eee41c04d2c1003c0bf86e2ec06
mbustos <mbustos@trazalog.com>
2020-01-24 15:24:51 -0300

    Conflicto menu arreglado

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -1,25 +1,6 @@
 <ul class="sidebar-menu menu tree" data-widget="tree">
     <li class="header"></li>
     <li><a class="link" href="#" data-link="traz-comp-bpm/Tarea"><i class="fa fa-list-alt"></i>Mis Tareas</a></li>
-<<<<<<< Updated upstream
-    <?php   $this->load->view('layout/aux_menu_alm') ?>
-
-    <!-- <li><a class="link" href="#" data-link="general/Etapa/fraccionar"><i class="fa fa-circle-o"></i>Fraccionar</a></li> -->
-
-    <li class="treeview">
-        <a href="#">
-            <i class="fa fa-fw fa-tasks"></i> <span>Producción</span>
-            <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-            </span>
-        </a>
-        <ul class="treeview-menu">
-            <li><a class="link" href="#" data-link="general/Etapa"><i class="fa fa-sitemap"></i>Etapas</a></li>
-            <li><a class="link" href="#" data-link="general/Reporte/tareasOperario"><i
-                        class="fa fa-arrow-left"></i>Producción</a>
-            </li>
-        </ul>
-=======
     <li><a class="link" href="#" data-link="general/Etapa"><i class="fa fa-sitemap"></i>Etapas</a></li>
     <li><a class="link" href="#" data-link="general/Etapa/fraccionar"><i class="fa fa-circle-o"></i>Fraccionar</a></li>
     <li><a class="link" href="#" data-link="general/Camion/salidaCamion"><i class="fa fa-circle-o"></i>Salida Camión</a>
@@ -29,7 +10,6 @@
     <li><a class="link" href="#" data-link="general/Camion/salidaCamion"><i class="fa fa-circle-o"></i>Descarga Camión</a></li>
     <li><a class="link" href="#" data-link="general/Camion/recepcionCamion"><i class="fa fa-exchange"></i>Carga | Recepción</a>
     <li><a class="link" href="#" data-link="general/Camion/cargadeCamion"><i class="fa fa-arrow-left"></i>Listado Carga Camion</a>
->>>>>>> Stashed changes
     </li>
 
     <li class="treeview">
@@ -64,16 +44,9 @@
         </a>
         <ul class="treeview-menu" style="display: none;">
             <!-- <li><a href="#" class="link" data-link="Reportes/index"><i class="fa fa-circle-o"></i>Ejemplo</a></li> -->
-<<<<<<< Updated upstream
-            <li><a href="#" class="link" data-link="Reportes/produccion"><i class="fa fa-circle-o"></i>Producción</a>
-            </li>
-            <li><a href="#" class="link" data-link="Reportes/prodResponsable"><i class="fa fa-circle-o"></i>Prod.
-                    Responsable</a></li>
-=======
             <li><a href="#" class="link" data-link="Reportes/produccion"><i class="fa fa-circle-o"></i>Producción</a></li>
             <li><a href="#" class="link" data-link="Reportes/prodResponsable"><i class="fa fa-circle-o"></i>Prod. Responsable</a></li>
             <!-- <li><a href="#" class="link" data-link="Reportes/tarjetas"><i class="fa fa-circle-o"></i>Tarjetas</a></li> -->
->>>>>>> Stashed changes
         </ul>
     </li>
 </ul>
