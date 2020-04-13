29a8b82183366369c302c75d790025a00685bcf2
Tincho Bustos <martingabriel.bustos@gmail.com>
2020-04-13 14:22:39 -0300

    ABM Fórmulas
    Alta, Baja y Modificación, más servicios de las Fórmulas (recetas)

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,7 +85,7 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 # DNATO
-define('LOGIN', true);
+define('LOGIN', false);
 define('DNATO','http://localhost/traz-comp-dnato/');
 
 #TRAZ-COMP-BPM
--- application/models/traz-comp-almacen/Articulos.php
+++ application/models/traz-comp-almacen/Articulos.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 
 class Articulos extends CI_Model
@@ -7,27 +7,24 @@ class Articulos extends CI_Model
 	{
 		parent::__construct();
 	}
-	
-	function getList()  
+
+	function getList()
 	{
 		$this->db->select('A.*, coalesce(sum(cantidad),0) as stock, T.valor');
 		$this->db->from('alm.alm_articulos A');
-		$this->db->join('alm.alm_lotes C', 'C.arti_id = A.arti_id','left');
-		$this->db->join('core.tablas T', 'A.tipo = T.tabl_id','left');
+		$this->db->join('alm.alm_lotes C', 'C.arti_id = A.arti_id', 'left');
+		$this->db->join('core.tablas T', 'A.tipo = T.tabl_id', 'left');
 		$this->db->where('A.empr_id', empresa());
 		$this->db->where('A.eliminado', false);
 		$this->db->group_by('A.arti_id, T.valor');
-			
-		
-		$query = $this->db->get();	
-		
-		
-		if ($query && $query->num_rows() > 0)
-		{
-			return $query->result();	
-		}
-		else
-		{	
+
+
+		$query = $this->db->get();
+
+
+		if ($query && $query->num_rows() > 0) {
+			return $query->result();
+		} else {
 			return array();
 		}
 	}
@@ -37,7 +34,7 @@ class Articulos extends CI_Model
 		$data['es_caja'] = isset($data['cantidad_caja']);
 		$data['es_loteado'] = isset($data['es_loteado']);
 		$data['empr_id'] = empresa();
-		$this->db->insert('alm.alm_articulos',$data);
+		$this->db->insert('alm.alm_articulos', $data);
 		return $this->db->insert_id();
 	}
 
@@ -49,93 +46,96 @@ class Articulos extends CI_Model
 
 	function get($id)
 	{
-		$this->db->where('arti_id',$id);
+		$this->db->where('arti_id', $id);
 		return $this->db->get('alm.alm_articulos')->row_array();
 	}
 
 	function getLotes($id)
 	{
-		$this->db->where('arti_id',$id);
+		$this->db->where('arti_id', $id);
 		$this->db->select('*');
 		$this->db->from('alm.alm_lotes T');
-		$this->db->join('alm.alm_depositos A','T.depo_id = A.depo_id');
+		$this->db->join('alm.alm_depositos A', 'T.depo_id = A.depo_id');
 		return $this->db->get()->result_array();
 	}
-	
+
 	function getpencil($id) // Ok
-    {
-    	$userdata  = $this->session->userdata('user_data');
+	{
+		$userdata  = $this->session->userdata('user_data');
 		$empresaId = empresa();
 
 		$this->db->select('A.*, B.tabl_id as unidadmedida,B.descripcion as unidad_descripcion');
 		$this->db->from('alm.alm_articulos A');
-		$this->db->join('alm.alm.utl_tablas B','A.unidad_id = B.tabl_id','left');
-		$this->db->where('arti_id',$id);
-		$this->db->where('empr_id',$empresaId);
+		$this->db->join('alm.alm.utl_tablas B', 'A.unidad_id = B.tabl_id', 'left');
+		$this->db->where('arti_id', $id);
+		$this->db->where('empr_id', $empresaId);
 
-		
 
-	    $query = $this->db->get();
-	    if( $query->num_rows() > 0)
-	    {
-	    	return $query->result_array();	
-	    } 
-	    else {
-	    	return 0;
-	    }
+
+		$query = $this->db->get();
+		if ($query->num_rows() > 0) {
+			return $query->result_array();
+		} else {
+			return 0;
+		}
 	}
 
-	function eliminar($id){
-		$this->db->where('arti_id',$id);
-		$this->db->set('eliminado',true);
+	function eliminar($id)
+	{
+		$this->db->where('arti_id', $id);
+		$this->db->set('eliminado', true);
 		return $this->db->update('alm.alm_articulos');
 	}
 
 	function getUnidadesMedidas()
 	{
-		$this->db->select('A.tabl_id as id_unidadmedida,A.descripcion');
-		$this->db->where('tabla','unidad');
+		$this->db->select('A.tabl_id as id_unidadmedida, A.descripcion, A.valor');
+		$this->db->where('tabla', 'unidad');
 		$query  = $this->db->get('alm.alm.utl_tablas A');
-		if($query->num_rows()>0)
-		{
-		    return $query->result_array();
+		if ($query->num_rows() > 0) {
+			return $query->result_array();
+		} else {
+			return false;
 		}
-		else
-		{
-		    return false;
-		}		
+	}
+
+	function getUM()
+	{
+		$this->db->select('A.tabl_id as id_unidadmedida, A.descripcion, A.valor');
+		$this->db->where('tabla', 'unidades_medida');
+		$query  = $this->db->get('alm.utl_tablas A');
+		if ($query->num_rows() > 0) {
+			return $query->result();
+		} else {
+			return false;
+		}
 	}
 
 
 	function getArticle($data = null)
 	{
-		
-		if($data == null || strpos('Add',$data['act']) == 0)
-		{
+
+		if ($data == null || strpos('Add', $data['act']) == 0) {
 			return false;
-		}
-		else
-		{
+		} else {
 			$userdata  = $this->session->userdata('user_data');
 			$empresaId = empresa();
 			$action    = $data['act'];
 			$idArt     = $data['id'];
 			$data      = array();
-			
+
 			$this->db->select('A.*,B.valor as unidad');
 			$this->db->from('alm.alm_articulos A');
-			$this->db->join('alm.alm.utl_tablas B','A.unidad_id = B.tabl_id');
-			$this->db->join('alm.alm.utl_tablas C','A.estado_id = C.tabl_id');
-			$this->db->where('C.valor','AC');
-		
-		    $query = $this->db->get();
-		    
-		 	if ($query->num_rows() != 0)
-			{
+			$this->db->join('alm.alm.utl_tablas B', 'A.unidad_id = B.tabl_id');
+			$this->db->join('alm.alm.utl_tablas C', 'A.estado_id = C.tabl_id');
+			$this->db->where('C.valor', 'AC');
+
+			$query = $this->db->get();
+
+			if ($query->num_rows() != 0) {
 				//echo "if ".$empresaId;
 				$c               = $query->result_array();
 				$data['article'] = $c[0];
-
 			} else {
 				//echo "else ".$empresaId;
 				$art                       = array();
@@ -156,25 +156,22 @@ class Articulos extends CI_Model
 			$data['article']['action'] = $action;
 			//Readonly
 			$readonly = false;
-			if($action == 'Del' || $action == 'View'){
+			if ($action == 'Del' || $action == 'View') {
 				$readonly = true;
 			}
 			$data['read']   = $readonly;
 			$data['action'] = $action;
-		
-			
+
+
 			return $data;
 		}
 	}
-	
+
 	function setArticle($data = null)
 	{
-		if($data == null)
-		{
+		if ($data == null) {
 			return false;
-		}
-		else
-		{
+		} else {
 			$userdata  = $this->session->userdata('user_data');
 			$empresaId = empresa();
 			$id        = $data['id'];
@@ -184,9 +181,9 @@ class Articulos extends CI_Model
 			$box       = $data['box'];
 			$boxCant   = $data['boxCant'];
 			$code      = $data['code'];
-		
-			$unidmed   = $data['unidmed'];   
-			$puntped   = $data['puntped'];     
+
+			$unidmed   = $data['unidmed'];
+			$puntped   = $data['puntped'];
 			$data      = array(
 				'barcode'     => $code,
 				'descripcion' => $name,
@@ -199,69 +196,68 @@ class Articulos extends CI_Model
 				'empr_id'	 => $empresaId
 			);
 
-			switch($act){
+			switch ($act) {
 				case 'Add':
-					if($this->db->get_where('alm.alm_articulos',['barcode'=>$code, 'empr_id'=>$empresaId])->num_rows()>0)return false;
-				
-					if($this->db->insert('alm.alm_articulos', $data)) {
+					if ($this->db->get_where('alm.alm_articulos', ['barcode' => $code, 'empr_id' => $empresaId])->num_rows() > 0) return false;
+
+					if ($this->db->insert('alm.alm_articulos', $data)) {
 						return $this->db->insert_id();
-					} 
+					}
 					break;
 				case 'Edit':
-				 	//Actualizar Artículo
-				 	if($this->db->update('alm.alm_articulos', $data, array('artId'=>$id)) == false) {
-				 		return false;
-				 	}
-				 	break;
+					//Actualizar Artículo
+					if ($this->db->update('alm.alm_articulos', $data, array('artId' => $id)) == false) {
+						return false;
+					}
+					break;
 				case 'Del':
-				 	//Eliminar Artículo
-				 	if($this->db->delete('alm.alm_articulos', array('artId'=>$id)) == false) {
-				 		return false;
-				 	}
-				 	break;
+					//Eliminar Artículo
+					if ($this->db->delete('alm.alm_articulos', array('artId' => $id)) == false) {
+						return false;
+					}
+					break;
 			}
 			return true;
 		}
 	}
-	
 
-	
+
+
 	function getdatosfams()
 	{
 		$userdata  = $this->session->userdata('user_data');
-        $empresaId = empresa();
+		$empresaId = empresa();
 		$query     = $this->db->get_where('conffamily', array('id_empresa' => $empresaId));
-		if($query->num_rows()>0){
-		    return $query->result();
+		if ($query->num_rows() > 0) {
+			return $query->result();
+		} else {
+			return false;
 		}
-		else{
-		    return false;
-		    }
-				
 	}
-	
+
 	function update_articulo($data, $idarticulo)
 	{
 		$userdata           = $this->session->userdata('user_data');
-        $empresaId          = empresa();
-        $data['id_empresa'] = $empresaId;
-        $this->db->where('artId', $idarticulo);
-        $query = $this->db->update("articles",$data);
+		$empresaId          = empresa();
+		$data['id_empresa'] = $empresaId;
+		$this->db->where('artId', $idarticulo);
+		$query = $this->db->update("articles", $data);
 		return $query;
-    }
+	}
 
 
 
 	function update_editar($data, $id)
 	{
-        $this->db->where('arti_id', $id);
-        $query = $this->db->update("alm.alm_articulos",$data);
-        return $query;
-    }
+		$this->db->where('arti_id', $id);
+		$query = $this->db->update("alm.alm_articulos", $data);
+		return $query;
+	}
 
-	function searchByCode($data = null){
+	function searchByCode($data = null)
+	{
 		$str = '';
-		if($data != null){
+		if ($data != null) {
 			$str = $data['code'];
 		}
 
@@ -269,11 +265,10 @@ class Articulos extends CI_Model
 
 		$this->db->select('*');
 		$this->db->from('articles');
-		$this->db->where(array('artBarCode'=>$str, 'artEstado'=>'AC')); 
+		$this->db->where(array('artBarCode' => $str, 'artEstado' => 'AC'));
 		$query = $this->db->get();
-		if ($query->num_rows()!=0)
-		{
-			if($query->num_rows() > 1){
+		if ($query->num_rows() != 0) {
+			if ($query->num_rows() > 1) {
 				//Multiples coincidencias
 			} else {
 				//Unica coincidencia
@@ -282,16 +277,15 @@ class Articulos extends CI_Model
 
 				//Calcular precios 
 				$pUnit = $articles['artCoste'];
-				if($articles['artIsByBox'] == 1){
+				if ($articles['artIsByBox'] == 1) {
 					$pUnit = $articles['artCoste'] / $articles['artCantBox'];
 				}
 
-				if($articles['artMarginIsPorcent'] == 1){
+				if ($articles['artMarginIsPorcent'] == 1) {
 					$articles['pVenta'] = $pUnit + ($pUnit * ($articles['artMargin'] / 100));
 				} else {
 					$articles['pVenta'] = $pUnit + $articles['artMargin'];
 				}
-
 			}
 			return $articles;
 		}
@@ -299,9 +293,10 @@ class Articulos extends CI_Model
 		return $articles;
 	}
 
-	function searchByAll($data = null){
+	function searchByAll($data = null)
+	{
 		$str = '';
-		if($data != null){
+		if ($data != null) {
 			$str = $data['code'];
 		}
 
@@ -309,24 +304,23 @@ class Articulos extends CI_Model
 
 		$this->db->select('*');
 		$this->db->from('articles');
-		$this->db->where('artEstado','AC');
-		if($str != ''){
-			$this->db->like('artBarCode',$str);
-			$this->db->or_like('artDescription',$str);
+		$this->db->where('artEstado', 'AC');
+		if ($str != '') {
+			$this->db->like('artBarCode', $str);
+			$this->db->or_like('artDescription', $str);
 		}
 		$query = $this->db->get();
-		if ($query->num_rows()!=0)
-		{
-			foreach($query->result_array() as $a){
+		if ($query->num_rows() != 0) {
+			foreach ($query->result_array() as $a) {
 				$articles = $a;
 
 				//Calcular precios 
 				$pUnit = $articles['artCoste'];
-				if($articles['artIsByBox'] == 1){
+				if ($articles['artIsByBox'] == 1) {
 					$pUnit = $articles['artCoste'] / $articles['artCantBox'];
 				}
 
-				if($articles['artMarginIsPorcent'] == 1){
+				if ($articles['artMarginIsPorcent'] == 1) {
 					$articles['pVenta'] = $pUnit + ($pUnit * ($articles['artMargin'] / 100));
 				} else {
 					$articles['pVenta'] = $pUnit + $articles['artMargin'];
@@ -343,20 +337,27 @@ class Articulos extends CI_Model
 	{
 		$userdata  = $this->session->userdata('user_data');
 		$empresaId = empresa();
-		
+
 		$this->db->select('articles.artEstado, tbl_estado.estadoid, tbl_estado.descripcion');
 		$this->db->from('articles');
 		$this->db->join('tbl_estado', 'tbl_estado.estado = articles.artEstado');
 		$this->db->where('articles.id_empresa', $empresaId);
 		$this->db->group_by('articles.artEstado');
-		$query = $this->db->get();	
-		if ($query->num_rows()!=0)
-		{
-			return $query->result_array();	
-		}
-		else
-		{	
+		$query = $this->db->get();
+		if ($query->num_rows() != 0) {
+			return $query->result_array();
+		} else {
 			return array();
 		}
+	}
+
+	public function getArtiService()
+	{
+		$url =  REST . 'articulos';
+		$data = $this->rest->callApi('GET', $url);
+		if ($data['status']) {
+			$data['data'] = json_decode($data['data'])->materias->materia;
+		}
+		return $data['data'];
 	}
 }
