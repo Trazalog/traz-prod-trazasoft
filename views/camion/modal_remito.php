93bc59afb0ad4a3b40c6fb51e35fb8157953443a
Pablo Marrelli <pmarrelli@trazalog.com>
2025-01-17 17:13:21 -0300

    ARMO-->hot-fix arreglo en produccion
    ARMO-->Logística-->Carga camión: que muestre el listado completo de artículos seleccionados (#457).-

--- views/camion/modal_remito.php
+++ views/camion/modal_remito.php
@@ -96,8 +96,13 @@ async function generaRemito() {
             wo();
             var clientesProductos = {};
             var listadoClientesCargados = [];
-            $('#tablacargas tbody tr').each(function() {
-                var jsonDecoded = JSON.parse($(this).attr("data-json"));
+
+            //console.log("Cantidad de datos en la tabla:");
+            //console.log($("#tabla_carga").DataTable().rows().count()); // Imprime el número total de filas
+
+            // Iterar sobre todas las filas
+            $('#tabla_carga').DataTable().rows().every(function() {
+                var jsonDecoded = JSON.parse($(this.node()).attr("data-json"));
 
                 if (!clientesProductos[jsonDecoded.cliente]) {
                     clientesProductos[jsonDecoded.cliente] = [];
@@ -126,11 +131,13 @@ async function generaRemito() {
             var modalBody = $('#modalBodyRemito');
             modalBody.find('#tabla_detalle tbody').empty();
             var total = 0;
-            //Cargo la data en el remito
+
+            // Formatear la fecha del remito
             var fechaUnformatted = $("#fecha").val();
             var fechaSpliteada = fechaUnformatted.split('-');
             var formattedDate = fechaSpliteada[2] + '-' + fechaSpliteada[1] + '-' + fechaSpliteada[0];
             $("#fechaRemito").text(formattedDate);
+            // Iterar sobre los clientes cargados y sus productos
             for (const cliente of listadoClientesCargados) {
                 modalBody.find('#clienteRemito').text(cliente.nombre);
                 clientesProductos[cliente.id].forEach(function(producto) {
@@ -150,10 +157,12 @@ async function generaRemito() {
                 // Formatear el total con el signo $ y dos decimales
                 var totalFormateado = '$' + total.toFixed(2);
                 modalBody.find('#footer_table2').val(totalFormateado);
-                //Obtengo el nro del contador para remito desde core.tablas
-                var remitoID = await guardaRemito(clientesProductos[cliente.id],cliente.id);
+
+                // Obtener el número del contador para remito desde core.tablas
+                var remitoID = await guardaRemito(clientesProductos[cliente.id], cliente.id);
                 $("#nroRemito").text(remitoID);
-                //Espero a que se resuelva la promesa para continuar las impresiones
+
+                // Esperar a que se resuelva la promesa para continuar las impresiones
                 await imprimirRemito();
 
                 modalBody.find('#tabla_detalle tbody').empty();
